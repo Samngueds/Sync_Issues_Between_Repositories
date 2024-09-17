9896391ac247e9bfa49a47c4f01b1d23108c894c
Samuel Gueds <125109662+Samngueds@users.noreply.github.com>
2024-09-17 18:09:05 -0300

    Update sync_issues.rb

--- sync_issues.rb
+++ sync_issues.rb
@@ -3,15 +3,12 @@ require 'octokit'
 # Recebe os tokens e nomes dos repositórios do ambiente
 public_repo_token = ENV['PUBLIC_REPO_TOKEN']
 private_repo_token = ENV['PRIVATE_REPO_TOKEN']
-central_repo_token = ENV['CENTRAL_REPO_TOKEN']  # Token do repositório central de issues
-public_repo = ENV['PUBLIC_REPO']
-private_repo = ENV['PRIVATE_REPO']
-central_repo = ENV['CENTRAL_REPO']  # Nome do repositório central de issues
+public_repo = ENV['PUBLIC_REPO']     # Nome do repositório público (ex: "usuario/repo_publico")
+private_repo = ENV['PRIVATE_REPO']   # Nome do repositório privado (central)
 
 # Configura os clientes Octokit para os repositórios
 public_client = Octokit::Client.new(access_token: public_repo_token)
 private_client = Octokit::Client.new(access_token: private_repo_token)
-central_client = Octokit::Client.new(access_token: central_repo_token)
 
 def fetch_issues(client, repo)
   puts "Buscando issues do repositório #{repo}..."
@@ -39,9 +36,7 @@ def synchronize_issues(source_client, source_repo, target_client, target_repo)
   target_issues = fetch_issues(target_client, target_repo)
 
   source_issues.each do |issue|
-    if target_issues.any? { |i| i[:title] == issue[:title] }
-      puts "Issue '#{issue[:title]}' já existe no repositório de destino. Pulando..."
-    else
+    unless target_issues.any? { |i| i[:title] == issue[:title] }
       create_issue(target_client, target_repo, issue[:title], issue[:body])
     end
   end
@@ -51,14 +46,8 @@ end
 
 puts "Iniciando sincronização de issues..."
 
-# Sincroniza do repositório público para o repositório central
-synchronize_issues(public_client, public_repo, central_client, central_repo)
+# Sincroniza do repositório público para o repositório privado (central)
+synchronize_issues(public_client, public_repo, private_client, private_repo)
 
-# Sincroniza do repositório privado para o repositório central
-synchronize_issues(private_client, private_repo, central_client, central_repo)
-
-# Sincroniza do repositório central para o repositório público
-synchronize_issues(central_client, central_repo, public_client, public_repo)
-
-# Sincroniza do repositório central para o repositório privado
-synchronize_issues(central_client, central_repo, private_client, private_repo)
+# Sincroniza do repositório privado (central) para o repositório público
+synchronize_issues(private_client, private_repo, public_client, public_repo)
